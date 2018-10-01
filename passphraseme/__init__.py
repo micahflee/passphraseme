7131dedeb83ff4690ab49ee72a20b6103c87e755
42B <42B@protonmail.com>
2018-10-01 14:44:06 -0600

    Use `FileType('r')` to guarantee custom wordlist file exists

--- passphraseme/__init__.py
+++ passphraseme/__init__.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
-import sys
-import os
 import inspect
+import os
 from secrets import choice
 
 
 def generate(filename, word_count=7):
-    try:
-        with open(filename) as f:
-            words = [line.rstrip('\n') for line in f]
-    except FileNotFoundError:
-        print("The wordlist filename doesn't exist.")
-        sys.exit()
+    with open(filename) as f:
+        words = [line.rstrip('\n') for line in f]
 
     return ' '.join(choice(words) for _ in range(word_count))
 
@@ -24,11 +19,11 @@ def main():
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-s1', '--short1', action="store_true", help="Use EFF's general short wordlist")
     group.add_argument('-s2', '--short2', action="store_true", help="Use EFF's short wordlist with unique prefixes")
-    group.add_argument('-d', '--dictionary', nargs='?', metavar='dictionary', help='Custom wordlist filename')
+    group.add_argument('-d', '--dictionary', nargs='?', metavar='dictionary', type=argparse.FileType('r'), help='Custom wordlist filename')
     args = parser.parse_args()
 
     if args.dictionary is not None:
-        filename = args.dictionary
+        filename = args.dictionary.name
     else:
         wordlists_path = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), 'wordlists')
 
